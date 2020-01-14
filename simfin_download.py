b28144a369ce15cc6b83a737658afd8f131bbf52
Brent Morrison <brent.morrison@hotmail.com>
2020-01-14 23:49:50 +0000

    testing git

---
+++ simfin_download.py
@@ -0,0 +1,20 @@
+# Simfin download script
+x = 1
+y = 2
+print(x + y)
+
+###import simfin as sf
+
+# Set your API-key for downloading data. This key gets the free data.
+###sf.set_api_key('free')
+
+# Set the local directory where data-files are stored.
+# The directory will be created if it does not already exist.
+###sf.set_data_dir('~/simfin_data/')
+
+# Download the data from the SimFin server and load into a Pandas DataFrame.
+###df = sf.load_balance(variant='quarterly', market='us')
+
+# Print the first rows of the data.
+###print(df.head())
+### adada
