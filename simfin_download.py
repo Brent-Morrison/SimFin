e60dffde062e23d5a2aff89569c42e4927d4bb31
Brent Morrison <brent.morrison@hotmail.com>
2020-02-12 01:35:22 +0000

    Up to date

--- simfin_download.py
+++ simfin_download.py
@@ -2,7 +2,12 @@
 
 # !pip install simfin
 
+import os
 import simfin as sf
+from simfin.names import *
+
+# Where is the saved path?
+print(os.path.expanduser('~'))
 
 # Set your API-key for downloading data. This key gets the free data.
 sf.set_api_key('free')
@@ -12,7 +17,12 @@ sf.set_api_key('free')
 sf.set_data_dir('~/simfin_data/')
 
 # Download the data from the SimFin server and load into a Pandas DataFrame.
-df = sf.load_balance(variant='quarterly', market='us')
+df_companies = sf.load_companies(market = 'us')
+df_industries = sf.load_industries()
+df_income = sf.load_income(variant = 'quarterly', market = 'us')
+df_balance = sf.load_balance(variant = 'quarterly', market = 'us')
+df_cashflow = sf.load_cashflow(variant = 'quarterly', market = 'us')
+df_prices = sf.load_shareprices(variant = 'daily', market = 'us')
 
 # Print the first rows of the data.
-print(df.head())
+print(df_prices.head())
