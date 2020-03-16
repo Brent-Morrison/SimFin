a766e95bdafbe4705c52d127c555eddb64a49b0b
Brent Morrison <brent.morrison@hotmail.com>
2020-03-16 21:19:21 +0000

    fmp cloud download

---
+++ simfin_data_prep.py
@@ -0,0 +1,12 @@
+# Imports
+import pandas as pd
+
+file_loc = 'C:/Users/brent/simfin_data/'
+
+industries = pd.read_csv(file_loc + 'industries.csv', sep = ';', index_col = 0)
+us_companies = pd.read_csv(file_loc + 'us-companies.csv', sep = ';', index_col = 0)
+us_balance_sheet = pd.read_csv(file_loc + 'us-balance-quarterly.csv', sep = ';', index_col = 0)
+us_income_statement = pd.read_csv(file_loc + 'us-income-quarterly.csv', sep = ';', index_col = 0)
+us_cashflow = pd.read_csv(file_loc + 'us-cashflow-quarterly.csv', sep = ';', index_col = 0)
+
+test = us_balance_sheet.loc['ORCL']
