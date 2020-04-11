4d6e0ffe20ff59d2039c6d286e2dd8aa68156b29
Brent Morrison <brent.morrison@hotmail.com>
2020-04-11 12:44:31 +0100

    minor changes

--- fmp_import.py
+++ fmp_import.py
@@ -8,15 +8,32 @@ import json
 # https://stackoverflow.com/questions/53558837/python-loop-to-pull-api-data-for-iterating-urls
 
 
+# Test implementation
 ticker_list = 'ABM' #['ORCL', 'ABM', 'ALJJ', 'TRN']
 url = 'https://fmpcloud.io/api/v3/'
 report = 'balance-sheet-statement/'
 apikey = '96a549a2823d1e4a3a66379d2868f0ec'
 
 #r = requests.get(url + report + ticker_list + ?period=quarter&apikey=96a549a2823d1e4a3a66379d2868f0ec')
-r = requests.get('https://fmpcloud.io/api/v3/balance-sheet-statement/ORCL?period=quarter&apikey=96a549a2823d1e4a3a66379d2868f0ec')
+r = requests.get('https://fmpcloud.io/api/v3/balance-sheet-statement/TRN?period=quarter&apikey=96a549a2823d1e4a3a66379d2868f0ec')
 
 df = pd.read_json(json.dumps(r.json()))
+
+obj = r.json()
+print(r.status_code)
+print(r == None)
+print(r.json())
+print(r.json() == None)
+
+if len(r.json()) != 0:
+
+
+
+
+
+
+
+
 
 # Looped implementation
 ticker_list = ['ORCL', 'ABM', 'ALJJ', 'TRN']
@@ -24,10 +41,11 @@ result = None
 
 for ticker in ticker_list:
     r = requests.get('https://fmpcloud.io/api/v3/balance-sheet-statement/{}?period=quarter&apikey=96a549a2823d1e4a3a66379d2868f0ec'.format(ticker))
-    r_df = pd.read_json(json.dumps(r.json()))
-    if result is None:
-        result = r_df
-    else:
-        result = np.concatenate((result, r_df))
+    if len(r.json()) != 0:
+        r_df = pd.read_json(json.dumps(r.json()))
+        if result is None:
+            result = r_df
+        else:
+            result = np.concatenate((result, r_df))
 
 print(r.url)
--- simfin_data_prep.py
+++ simfin_data_prep.py
@@ -8,5 +8,8 @@ us_companies = pd.read_csv(file_loc + 'us-companies.csv', sep = ';', index_col =
 us_balance_sheet = pd.read_csv(file_loc + 'us-balance-quarterly.csv', sep = ';', index_col = 0)
 us_income_statement = pd.read_csv(file_loc + 'us-income-quarterly.csv', sep = ';', index_col = 0)
 us_cashflow = pd.read_csv(file_loc + 'us-cashflow-quarterly.csv', sep = ';', index_col = 0)
+us_shareprices = pd.read_csv(file_loc + 'us-shareprices-daily.csv', sep = ';', index_col = 0)
 
-test = us_balance_sheet.loc['ORCL']
+test = us_shareprices.loc['ORCL']
+
+test1 = test.groupby('Ticker').resample('M', how = 'sum')
